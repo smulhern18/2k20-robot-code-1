be694b00962b606ba58ec90c006b9cd1b4a55c27
GrantPerkins <gcperk20@gmail.com>
2020-01-22 01:04:06 -0500

    Move TestAutoCommand to proper package

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,7 +12,7 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.DrivetrainConstants;
-import frc.robot.commands.TestAutoCommand;
+import frc.robot.commands.auto.TestAutoCommand;
 import frc.robot.commands.drivetrain.DefaultDriveCommand;
 import frc.robot.commands.shooter.DefaultShootCommand;
 import frc.robot.input.AttackThree;
