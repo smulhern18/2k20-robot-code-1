32c9055d1159636c6c584a81f27c739f89295ba3
GrantPerkins <gcperk20@gmail.com>
2020-01-21 22:06:57 -0500

    Created Velocity class to clarify unit conversions Also changed how shooting works, you must set target velocity, then call shoot() to run to that speed. Likely a safer approach

--- src/main/java/frc/robot/commands/shooter/ShootCommand.java
+++ src/main/java/frc/robot/commands/shooter/ShootCommand.java
@@ -1,28 +1,43 @@
 package frc.robot.commands.shooter;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.ShooterSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
+/**
+ * Reads velocity from Shuffleboard, runs shooter at that speed
+ */
 public class ShootCommand extends CommandBase {
 
   private final ShooterSubsystem shooterSubsystem;
+  private final String SETPOINT = "Shooter setpoint RPM";
 
+  /**
+   * Puts 0 rpm to shuffleboard
+   * @param shooterSubsystem the shooter subsystem
+   */
   public ShootCommand(ShooterSubsystem shooterSubsystem) {
     addRequirements(shooterSubsystem);
     this.shooterSubsystem = shooterSubsystem;
+    SmartDashboard.putNumber(SETPOINT, shooterSubsystem.targetVelocity.getRPM());
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
+  /**
+   * Reads current target, runs shooter to that velocity
+   */
   @Override
   public void execute() {
-    double velocity = shooterSubsystem.getSetpointVelocity();
-    shooterSubsystem.shoot(velocity);
+    shooterSubsystem.setTargetRPM(SmartDashboard.getNumber(SETPOINT, 0));
+    shooterSubsystem.shoot();
   }
 
-  // Called once the command ends or is interrupted.
+  /**
+   * Stops shooter wheel
+   * @param interrupted unused
+   */
   @Override
   public void end(boolean interrupted) {
-    shooterSubsystem.shoot(0);
+    shooterSubsystem.stop();
   }
 
 }
