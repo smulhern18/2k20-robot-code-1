e6fa670d3aee6f4ae8dd987fc3ac98739f0de9b4
Alexander Sun <technoal9@gmail.com>
2020-01-23 18:35:47 -0500

    LED first version!

--- src/main/java/frc/robot/commands/blinkinpark/PlaySongCommand.java
+++ src/main/java/frc/robot/commands/blinkinpark/PlaySongCommand.java
@@ -1,12 +1,23 @@
 package frc.robot.commands.blinkinpark;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.BlinkinParkSubsystem;
+import frc.robot.subsystems.BlinkinParkSubsystem.Song;
 
 public class PlaySongCommand extends CommandBase{
 
+    private Song song;
+    private BlinkinParkSubsystem blinkinPark;
+
+    public PlaySongCommand(Song song, BlinkinParkSubsystem blinkinPark){
+        this.song = song;
+        this.blinkinPark = blinkinPark;
+        addRequirements(blinkinPark);
+    }
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
+        blinkinPark.playSong(song);
     }
 
     // Called every time the scheduler runs while the command is scheduled.
