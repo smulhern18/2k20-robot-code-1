0a5dc68d08e20299fb6301a255937031c578f2eb
Alexander Sun <technoal9@gmail.com>
2020-01-25 13:49:11 -0500

    added trajectory packages and classes

---
+++ src/main/java/frc/robot/commands/auto/rendezvous/RendezvousTrajectories.java
@@ -0,0 +1,4 @@
+package frc.robot.commands.auto.rendezvous;
+
+public class RendezvousTrajectories {
+}
---
+++ src/main/java/frc/robot/commands/auto/trench/TrenchAutoCommand.java
@@ -0,0 +1,4 @@
+package frc.robot.commands.auto.trench;
+
+public class TrenchAutoCommand {
+}
