960e2a67126be24d45f8e9338b3b38f9a3822e12
Alexander Sun <technoal9@gmail.com>
2020-01-25 14:26:24 -0500

    Trench auto command added

--- src/main/java/frc/robot/commands/auto/trench/TrenchAutoCommand.java
+++ src/main/java/frc/robot/commands/auto/trench/TrenchAutoCommand.java
@@ -1,4 +1,12 @@
 package frc.robot.commands.auto.trench;
 
-public class TrenchAutoCommand {
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.drivetrain.TrajectoryFollowerCommand;
+import frc.robot.subsystems.DrivetrainSubsystem;
+
+public class TrenchAutoCommand extends SequentialCommandGroup {
+    public TrenchAutoCommand(DrivetrainSubsystem drivetrain){
+        addCommands(new TrajectoryFollowerCommand(TrenchTrajectories.GRAB, drivetrain),
+                new TrajectoryFollowerCommand(TrenchTrajectories.RETURN, drivetrain));
+    }
 }
--- src/main/java/frc/robot/commands/auto/trench/TrenchTrajectories.java
+++ src/main/java/frc/robot/commands/auto/trench/TrenchTrajectories.java
@@ -1,4 +1,21 @@
 package frc.robot.commands.auto.trench;
 
-public class TrenchTrajectories {
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryGenerator;
+import frc.robot.commands.auto.Trajectories;
+
+import java.util.List;
+
+public final class TrenchTrajectories extends Trajectories {
+    public static final Trajectory GRAB = TrajectoryGenerator.generateTrajectory(
+            List.of(new Pose2d(3.5,-0.75, new Rotation2d(0)),
+                    new Pose2d(9.75,-0.75, new Rotation2d(0))),
+            BACKWARD_CONFIG);
+
+    public static final Trajectory RETURN = TrajectoryGenerator.generateTrajectory(
+            List.of(new Pose2d(9.75,-0.75, new Rotation2d(0)),
+                    new Pose2d(3.5,-0.75, new Rotation2d(0))),
+            FORWARD_CONFIG);
 }
