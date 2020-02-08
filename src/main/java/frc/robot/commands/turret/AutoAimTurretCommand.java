91c7c40a0387696206cbfccc244caf1c09fb1336
GrantPerkins <gcperk20@gmail.com>
2020-02-08 13:21:55 -0500

    Create CollectCommand, rename turret commands

--- src/main/java/frc/robot/commands/turret/AutoAimTurretCommand.java
+++ src/main/java/frc/robot/commands/turret/AutoAimTurretCommand.java
@@ -6,18 +6,17 @@ import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.TurretSubsystem;
 import frc.robot.subsystems.VisionSubsystem;
 
-public class DefaultVisionFollowCommand extends CommandBase {
+public class AutoAimTurretCommand extends CommandBase {
     private final TurretSubsystem turretSubsystem;
     private final VisionSubsystem visionSubsystem;
     private final DrivetrainSubsystem drivetrainSubsystem;//need for odometry
 
-    DefaultVisionFollowCommand(TurretSubsystem turretSubsystem, VisionSubsystem visionSubsystem, DrivetrainSubsystem drivetrainSubsystem){
-        addRequirements(turretSubsystem, visionSubsystem, drivetrainSubsystem);
-
+    AutoAimTurretCommand(TurretSubsystem turretSubsystem, VisionSubsystem visionSubsystem, DrivetrainSubsystem drivetrainSubsystem){
         this.turretSubsystem = turretSubsystem;
         this.visionSubsystem = visionSubsystem;
+        //drivetrain not required because it's only being used to read from NavX
         this.drivetrainSubsystem = drivetrainSubsystem;
-
+        addRequirements(turretSubsystem, visionSubsystem);
     }
 
     @Override
--- src/main/java/frc/robot/subsystems/CollectorSubsystem.java
+++ src/main/java/frc/robot/subsystems/CollectorSubsystem.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.Solenoid;
 import frc.robot.Constants.CollectorConstants;
 
 public class CollectorSubsystem extends BeefSubsystemBase {
@@ -9,19 +10,52 @@ public class CollectorSubsystem extends BeefSubsystemBase {
    * Creates a new CollectorSubsystem.
    */
   private WPI_TalonSRX collectorMotor;
+  private Solenoid collectorDeployPiston;
+  private CollectorState state;
 
   public CollectorSubsystem() {
     collectorMotor = new WPI_TalonSRX(CollectorConstants.COLLECTOR_MOTOR_CHANNEL);
+    collectorDeployPiston = new Solenoid(CollectorConstants.DEPLOY_COLLECTOR_SOLENOID_CHANNEL);
+    state = CollectorState.UNDEPLOYED;
   }
 
   @Override
-  public void periodic(){
+  public void periodic() {
 
   }
 
-  public void collect(){//collects balls
-    collectorMotor.set(ControlMode.PercentOutput, 0.48);
+  public void deploy() {
+    collectorDeployPiston.set(true);
+    state = CollectorState.DEPLOYED;
   }
 
+  public void undeploy() {
+    collectorDeployPiston.set(false);
+    state = CollectorState.UNDEPLOYED;
+  }
 
+  public void intake() {
+    collectorMotor.set(1);
+  }
+
+  public void exhaust() {
+    collectorMotor.set(-1);
+  }
+
+  public void stopIntake() {
+    collectorMotor.set(0);
+  }
+
+  public CollectorState getState() {
+    return state;
+  }
+
+  public void setState(CollectorState state) {
+    this.state = state;
+  }
+
+  public enum CollectorState {
+    DEPLOYED,
+    UNDEPLOYED
+  }
 }
