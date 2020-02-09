c8893405ea1d008ab0fa2937f975c817bae346a0
Alexander Sun <technoal9@gmail.com>
2020-02-08 23:58:06 -0500

    one minor fix

--- src/main/java/frc/robot/commands/turret/AutoAimTurretCommand.java
+++ src/main/java/frc/robot/commands/turret/AutoAimTurretCommand.java
@@ -2,6 +2,7 @@ package frc.robot.commands.turret;
 
 import edu.wpi.first.wpilibj.util.Units;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
 import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.TurretSubsystem;
 import frc.robot.subsystems.VisionSubsystem;
@@ -27,7 +28,7 @@ public class AutoAimTurretCommand extends CommandBase {
   @Override
   public void execute() {
     if (visionSubsystem.getTargetFound()) {
-      turretSubsystem.rotateToPosition(Units.degreesToRadians(135)+ visionSubsystem.getAngleToTarget());//adjusts for middle offset
+      turretSubsystem.rotateToPosition(Units.degreesToRadians(Constants.TurretConstants.MAX_CAPABILITY_DEGREES / 2.0)+ visionSubsystem.getAngleToTarget());//adjusts for middle offset
     } else {//currently unsure with where to point shooter with no vision
       turretSubsystem.setTargetPosition(-1.0 * drivetrainSubsystem.getYawDegrees());//TODO check with odomoetry for -180 to 180 or 0 to 360
     }
