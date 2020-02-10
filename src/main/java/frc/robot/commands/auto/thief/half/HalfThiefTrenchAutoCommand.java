bebaefcfe099e5d4fc33c6c4529611a73ef946bb
GrantPerkins <gcperk20@gmail.com>
2020-02-10 16:11:26 -0500

    Create HalfThiefTrenchAutoCommand

--- src/main/java/frc/robot/commands/auto/thief/half/HalfThiefTrenchAutoCommand.java
+++ src/main/java/frc/robot/commands/auto/thief/half/HalfThiefTrenchAutoCommand.java
@@ -1,14 +1,35 @@
 package frc.robot.commands.auto.thief.half;
 
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.RobotContainer;
+import frc.robot.commands.collector.CollectCommand;
 import frc.robot.commands.drivetrain.TrajectoryFollowerCommand;
+import frc.robot.commands.shooter.AutoAimAndShootCommand;
+import frc.robot.commands.trenchable.UntrenchCommand;
 
 public class HalfThiefTrenchAutoCommand extends SequentialCommandGroup {
   public HalfThiefTrenchAutoCommand(RobotContainer robotContainer) {
     addCommands(
-        new TrajectoryFollowerCommand(robotContainer, HalfThiefTrenchTrajectories.FIRST_TWO),
-        new TrajectoryFollowerCommand(robotContainer, HalfThiefTrenchTrajectories.SHOOT_ONE),
-        new TrajectoryFollowerCommand(robotContainer, HalfThiefTrenchTrajectories.UNDER_RENDEZ));
+        // Collect two balls from enemy trench
+        new ParallelCommandGroup(
+            new CollectCommand(robotContainer).withTimeout(3),
+            new TrajectoryFollowerCommand(robotContainer, HalfThiefTrenchTrajectories.FIRST_TWO)
+        ),
+        // move to port and untrench
+        new ParallelCommandGroup(
+            new UntrenchCommand(robotContainer),
+            new TrajectoryFollowerCommand(robotContainer, HalfThiefTrenchTrajectories.SHOOT_ONE)
+        ),
+        // shoot 5 balls
+        new AutoAimAndShootCommand(robotContainer),
+        // Collect 5 balls
+        new ParallelCommandGroup(
+            new CollectCommand(robotContainer).withTimeout(8),
+            new TrajectoryFollowerCommand(robotContainer, HalfThiefTrenchTrajectories.UNDER_RENDEZ)
+        ),
+        // Shoot 5 balls
+        new AutoAimAndShootCommand(robotContainer)
+    );
   }
 }
