3373f3b19d75c320614a401fdfbf154534dd761c
GrantPerkins <gcperk20@gmail.com>
2020-02-14 18:24:24 -0500

    Remove unused constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -122,11 +122,6 @@ public final class Constants {
     public final static double DEFAULT_RPM = 7200;
 
     public final static String VELOCITY_ENTRY = "Shooter Velocity";
-    public final static String TARGET_ENTRY = "Shooter Target RPM";
-    public final static String CONFIG_P_ENTRY = "Config P";
-    public final static String CONFIG_I_ENTRY = "Config I";
-    public final static String CONFIG_D_ENTRY = "Config D";
-    public final static String CONFIG_F_ENTRY = "Config F";
   }
 
   public static class VisionConstants {
@@ -166,7 +161,6 @@ public final class Constants {
     public final static double POT_MIN = 0;
     public final static double POT_MAX = 2048; // TODO: configure
     public final static double MAX_ROTATION_DEGREES = 270.0;
-    public final static double MAX_POT_ROTATIONS = 10;
 
     public final static double ERROR_TOLERANCE = 0.50;// set to real value
   }
