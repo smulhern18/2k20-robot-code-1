c4ac61f9a73c62b5ca69fe042ff20585f77cdd35
Alexander Sun <technoal9@gmail.com>
2020-02-15 18:09:15 -0500

    added more

--- src/main/java/frc/robot/commands/ballpath/shift/ShiftFourthToFifthCommand.java
+++ src/main/java/frc/robot/commands/ballpath/shift/ShiftFourthToFifthCommand.java
@@ -4,6 +4,9 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.BallPathSubsystem;
 
+/**
+ * Shifts the ball path to create space and index for five balls
+ */
 public class ShiftFourthToFifthCommand extends CommandBase {
   BallPathSubsystem ballPathSubsystem;
 
@@ -12,11 +15,19 @@ public class ShiftFourthToFifthCommand extends CommandBase {
     addRequirements(ballPathSubsystem);
   }
 
+  /**
+   * Runs the indexer
+   */
   @Override
   public void execute() {
     ballPathSubsystem.runIndexer();
   }
 
+  /**
+   * Stops when any banner sensor is triggered, either the intended one, or any past it
+   *
+   * @return if the lead ball has reached or passed its intended point
+   */
   @Override
   public boolean isFinished() {
     return ballPathSubsystem.fifthCellBannerSensor.beamBroken();
