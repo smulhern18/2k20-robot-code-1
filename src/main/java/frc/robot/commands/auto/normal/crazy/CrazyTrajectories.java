5709df1292812bc9f44629331905cef859d195d3
Anthony Vuolo <34285458+LunarNebula@users.noreply.github.com>
2020-02-18 20:05:22 -0500

    Some reformatting of Crazy, Rendezvous autos

--- src/main/java/frc/robot/commands/auto/normal/crazy/CrazyTrajectories.java
+++ src/main/java/frc/robot/commands/auto/normal/crazy/CrazyTrajectories.java
@@ -10,22 +10,21 @@ import frc.robot.commands.auto.Trajectories;
 import java.util.List;
 
 public final class CrazyTrajectories extends Trajectories {
-  private static final Pose2d FIRST_THREE_START = new Pose2d(7.005, -0.894, new Rotation2d(Units.degreesToRadians(3.06)));
+  private static final Pose2d FIRST_THREE_START = new Pose2d(-3.284, -0.178, new Rotation2d(Units.degreesToRadians(3.06)));
   public static final Trajectory START = TrajectoryGenerator.generateTrajectory(
       List.of(
-          new Pose2d(3.721, -1.072, new Rotation2d(Units.degreesToRadians(3.08))),
+          new Pose2d(0.000, 0.000, new Rotation2d(Units.degreesToRadians(0.00))),
           FIRST_THREE_START
       ),
       BACKWARD_CONFIG);
-  private static final Pose2d LAST_TWO_START = new Pose2d(9.539, -0.715, new Rotation2d(Units.degreesToRadians(0.00)));
+  private static final Pose2d LAST_TWO_START = new Pose2d(-5.818, -0.357, new Rotation2d(Units.degreesToRadians(0.00)));
   public static final Trajectory FIRST_THREE = TrajectoryGenerator.generateTrajectory(
       List.of(
           FIRST_THREE_START,
-          new Pose2d(8.611, -0.787, new Rotation2d(Units.degreesToRadians(4.20))),
           LAST_TWO_START
       ),
       BACKWARD_CONFIG);
-  private static final Pose2d GRAB_TWO_START = new Pose2d(5.97, -2.215, new Rotation2d(Units.degreesToRadians(-71.13)));
+  private static final Pose2d GRAB_TWO_START = new Pose2d(-2.249, 1.143, new Rotation2d(Units.degreesToRadians(-71.13)));
   public static final Trajectory LAST_TWO = TrajectoryGenerator.generateTrajectory(
       List.of(
           LAST_TWO_START,
@@ -36,7 +35,7 @@ public final class CrazyTrajectories extends Trajectories {
   public static final Trajectory GRAB_TWO = TrajectoryGenerator.generateTrajectory(
       List.of(
           GRAB_TWO_START,
-          new Pose2d(6.196, -2.893, new Rotation2d(Units.degreesToRadians(-67.76)))
+          new Pose2d(-2.475, 1.821, new Rotation2d(Units.degreesToRadians(-67.76)))
       ),
       BACKWARD_CONFIG);
 
