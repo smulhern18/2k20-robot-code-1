dea041de0597cae67bccdf613e4c33ddb2ff5772
Alexander Sun <technoal9@gmail.com>
2020-02-19 19:30:50 -0500

    added opposite 3

--- src/main/java/frc/robot/commands/auto/thief/oppositethree/OppositeThreeTrajectories.java
+++ src/main/java/frc/robot/commands/auto/thief/oppositethree/OppositeThreeTrajectories.java
@@ -10,12 +10,12 @@ import frc.robot.commands.auto.Trajectories;
 import java.util.List;
 
 public final class OppositeThreeTrajectories extends Trajectories {
-  private static final Pose2d SHOOT_START = new Pose2d(10.188, -4.545, new Rotation2d(Units.degreesToRadians(-68.20)));
+  private static final Pose2d SHOOT_START = new Pose2d(-5.911, -2.876, new Rotation2d(Units.degreesToRadians(-68.20)));
   public static final Trajectory OPPOSITE_THREE_GRAB = TrajectoryGenerator.generateTrajectory(
       List.of(
-          new Pose2d(4.277, -7.421, new Rotation2d(Units.degreesToRadians(0.00))),
-          new Pose2d(6.824, -5.621, new Rotation2d(Units.degreesToRadians(46.11))),
-          new Pose2d(9.248, -3.581, new Rotation2d(Units.degreesToRadians(0.00))),
+          new Pose2d(0, 0, new Rotation2d(Units.degreesToRadians(0.00))),
+          new Pose2d(-2.547, -1.8, new Rotation2d(Units.degreesToRadians(46.11))),
+          new Pose2d(-4.971, -3.84, new Rotation2d(Units.degreesToRadians(0.00))),
           SHOOT_START
       ),
       BACKWARD_CONFIG);
@@ -23,7 +23,7 @@ public final class OppositeThreeTrajectories extends Trajectories {
   public static final Trajectory OPPOSITE_THREE_SHOOT = TrajectoryGenerator.generateTrajectory(
       List.of(
           SHOOT_START,
-          new Pose2d(6.000, -2.416, new Rotation2d(Units.degreesToRadians(0.00)))
+          new Pose2d(-1.323, -5.005, new Rotation2d(Units.degreesToRadians(0.00)))
       ),
       FORWARD_CONFIG);
 }
