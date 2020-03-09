a67cbcf377a3600146536e200ee18e69a219721c
GrantPerkins <gcperk20@gmail.com>
2020-03-09 10:42:04 -0400

    Post SE Mass

--- src/main/java/frc/robot/commands/climber/SlapCommand.java
+++ src/main/java/frc/robot/commands/climber/SlapCommand.java
@@ -1,6 +1,7 @@
 package frc.robot.commands.climber;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.RobotContainer;
 import frc.robot.commands.turret.TurretCommand;
@@ -9,7 +10,8 @@ public class SlapCommand extends SequentialCommandGroup {
   public SlapCommand(RobotContainer robotContainer) {
     addCommands(
 //        new TurretCommand(robotContainer, 0).withTimeout(2),
-        new InstantCommand(robotContainer.climberSubsystem::slap, robotContainer.climberSubsystem)
+        new InstantCommand(robotContainer.climberSubsystem::slap, robotContainer.climberSubsystem),
+        new PrintCommand("slapping")
     );
   }
 }
